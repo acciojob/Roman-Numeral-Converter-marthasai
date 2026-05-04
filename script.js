dc1ef5905420d568c8dddc910737e4e6c20faf7c
marthasai <158295414+marthasai@users.noreply.github.com>
2026-05-04 19:20:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
 
-  //your code here
-	if (num === 0) return 'N'; // Nulla for zero
-  
+  if (num === 0) return 'N';
+
+  const symbols = [
+    ['M',1000], 
+    ['CM',900],
+    ['D',500], 
+    ['CD',400],
+    ['C',100], 
+    ['XC',90],
+    ['L',50], 
+    ['XL',40],
+    ['X',10], 
+    ['IX',9],
+    ['V',5], 
+    ['IV',4],
+    ['I',1]
+  ];
+
   let result = '';
-  for (const [symbol, value] of symbols) {
+
+  for (let [symbol, value] of symbols) {
     while (num >= value) {
       result += symbol;
       num -= value;
     }
   }
+
   return result;
-
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+function convert() {
+  const num = Number(document.getElementById("numberInput").value);
 
+  if (isNaN(num)) {
+    document.getElementById("result").innerText = "Enter a valid number";
+    return;
+  }
 
+  document.getElementById("result").innerText = convertToRoman(num);
+}
 
-
-// do not edit below this line
-module.exports = convertToRoman
+// ✅ Safe export (won’t break browser)
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = convertToRoman;
+}
